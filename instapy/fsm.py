36b7e4c69ac57422c2f915c44b9e2488ffe1c855
christophe-duc <christophe@twilight-corp.com>
2019-07-27 09:53:45 -0300

    in between commits

--- instapy/fsm.py
+++ instapy/fsm.py
@@ -20,16 +20,25 @@ class FSMSession(Object):
 
     states = [
         'idle',
-        'follow',
-        'unfollow',
-        'like',
-        'comment',
-        'interact',
-        'watch',
-        'get_data'
+        'followed',
+        'unfollowed',
+        'liked',
+        'commented',
+        'interacted',
+        'watched',
+        'paused'
     ]
 
     transitions = [
+        {'trigger': 'do_idle', 'source': '*', 'dest': 'idle'},
+        {'trigger': 'do_follow', 'source': 'idle', 'dest': 'followed'},
+        {'trigger': 'do_unfollow', 'source': 'idle', 'dest': 'unfollowed'},
+        {'trigger': 'do_like', 'source': 'idle', 'dest': 'liked'},
+        {'trigger': 'do_comment', 'source': 'idle', 'dest': 'commented'},
+        {'trigger': 'do_interact', 'source': 'followed', 'dest': 'interacted'},
+        {'trigger': 'do_interact', 'source': 'liked', 'dest': 'interacted'},
+        {'trigger': 'do_watch', 'source': 'idle', 'dest': 'watched'},
+        {'trigger': 'do_pause', 'source': 'idle', 'dest': 'paused'},
 
     ]
 
@@ -42,29 +51,79 @@ class FSMSession(Object):
         "peak_server_calls": (300, 3500)
     }
 
-    #can this be generated from states automatically?
-    actions = {
-        "do_idle": {},
-        "do_follow": {},
-        "do_unfollow": {},
-        "do_like": {},
-        "do_comment": {},
-        "do_interact": {},
-        "do_watch": {},
-        "do_get_data": {}
-    }
+    machine = None
+
+    #stack of posts to do actions on
+    posts_stack_likes = {}
+    posts_stack_interact = {}
+    posts_stack_comment = {}
+    #stack of users to do action on
+    users_stack = {}
+    users_stack_follow = {}
+    users_stack_unfollow = {}
 
 
-    # def __init__(self, quota: dict = None, actions: dict = None):
-    #     """
-    #     Initialization of the FSM with what we want to do
-    #
-    #     :param quota: the quota information
-    #     :param actions: what we want to do
-    #     """
+    def __init__(self, quota: dict = None, actions: dict = None):
+        """
+        Initialization of the FSM with what we want to do
+
+        :param quota: the quota information
+        :param actions: what we want to do
+        """
+        self.machine = Machine(model=self, states=states, transitions=transitions, initial='idle')
+
+    def on_enter_idle(self):
+        """
+        check if we still have some quota
+        otherwise trigger pause
+        :return:
+        """
+        if random.randint(0,100) <50:
+            self.machine.trigger('do_pause')
+
+    def on_exit_idle(self):
+        """
+
+        :return:
+        """
+
+    def do_idle(self):
+        """
+        execute the actions needed to go to the next actions
+        ideally we randomize the states we want to go in and we trigger them
+        :return:
+        """
+
+        self.machine.trigger(random.sample(self.states,1))
+
+    def do_follow(self):
+        """
+        execute the actions needed to follow a user
+        :return:
+        """
+        print("doing the follow action")
+
+    def do_unfollow(self):
+        """
+        execute the actions needed to unfollow a user
+        :return:
+        """
+        print("doing the unfollow action")
+
+    def on_enter_followed(self):
+        """
+        we had a successfull follow
+        :return:
+        """
+        self.followed +=1
+        #update the db
+        #do all what is needed to do
+
+        #clean up
+        user=self.users_stack_follow.pop()
+        if Setting.do_interact:
+            self.users_stack_interact.append(user)
+        #more actions possible here if needed
 
 
-
-def main():
-    self.fsm_session = Machine('fsm_session', states=states, transitions=transitions, initial='idle')
 
