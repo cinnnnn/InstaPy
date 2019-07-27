f5fa1215ef1be592e5625655f47d38d7dfcb31cf
christophe-duc <christophe@twilight-corp.com>
2019-07-27 12:21:27 -0300

    intermediary commit

--- instapy/fsm.py
+++ instapy/fsm.py
@@ -18,6 +18,7 @@ class FSMSession(Object):
 
     """
 
+    # all_possible_states
     states = [
         'idle',
         'followed',
@@ -29,17 +30,18 @@ class FSMSession(Object):
         'paused'
     ]
 
+    # all possible transitions
     transitions = [
-        {'trigger': 'do_idle', 'source': '*', 'dest': 'idle'},
-        {'trigger': 'do_follow', 'source': 'idle', 'dest': 'followed'},
-        {'trigger': 'do_unfollow', 'source': 'idle', 'dest': 'unfollowed'},
-        {'trigger': 'do_like', 'source': 'idle', 'dest': 'liked'},
-        {'trigger': 'do_comment', 'source': 'idle', 'dest': 'commented'},
-        {'trigger': 'do_interact', 'source': 'followed', 'dest': 'interacted'},
-        {'trigger': 'do_interact', 'source': 'liked', 'dest': 'interacted'},
-        {'trigger': 'do_watch', 'source': 'idle', 'dest': 'watched'},
-        {'trigger': 'do_pause', 'source': 'idle', 'dest': 'paused'},
-
+        {'trigger': 'go_idle', 'source': '*', 'dest': 'idle'},
+        {'trigger': 'go_follow', 'source': 'idle', 'dest': 'followed'},
+        {'trigger': 'go_unfollow', 'source': 'idle', 'dest': 'unfollowed'},
+        {'trigger': 'go_like', 'source': 'idle', 'dest': 'liked'},
+        {'trigger': 'go_comment', 'source': 'idle', 'dest': 'commented'},
+        {'trigger': 'go_interact', 'source': ['followed', 'liked'], 'dest': 'interacted'},
+        {'trigger': 'go_watch', 'source': 'idle', 'dest': 'watched'},
+        {'trigger': 'go_pause', 'source': 'idle', 'dest': 'paused'},
+        {'trigger': 'do_gather_data', 'source': 'idle', 'dest': 'idle'},
+        {'trigger': 'do_work', 'source': 'idle', 'dest': '*'}
     ]
 
     #currently safe numbers
@@ -54,12 +56,13 @@ class FSMSession(Object):
     machine = None
 
     #stack of posts to do actions on
+    posts_stack = {}
     posts_stack_likes = {}
-    posts_stack_interact = {}
     posts_stack_comment = {}
     #stack of users to do action on
     users_stack = {}
     users_stack_follow = {}
+    users_stack_interact = {}
     users_stack_unfollow = {}
 
 
@@ -70,6 +73,10 @@ class FSMSession(Object):
         :param quota: the quota information
         :param actions: what we want to do
         """
+        #we should initialize the state machine according to the config
+        #no need to have followed states if following is not configured
+
+        print("auto-configure states and transitions here!")
         self.machine = Machine(model=self, states=states, transitions=transitions, initial='idle')
 
     def on_enter_idle(self):
@@ -79,7 +86,10 @@ class FSMSession(Object):
         :return:
         """
         if random.randint(0,100) <50:
-            self.machine.trigger('do_pause')
+            print("[FSM] - state {}=".format(self.machine.state))
+            self.machine.trigger('go_pause')
+        else:
+            self.machine.trigger('do_work')
 
     def on_exit_idle(self):
         """
@@ -87,23 +97,38 @@ class FSMSession(Object):
         :return:
         """
 
-    def do_idle(self):
+    def do_work(self):
         """
         execute the actions needed to go to the next actions
         ideally we randomize the states we want to go in and we trigger them
         :return:
         """
+        # here we should have in the state machine only the states we can do
+        # because we have data, otherwise the only state that it can go is in idle
+        next_transition="go_"+random.sample(self.states,1)
+        self.machine.trigger(next_transition)
 
-        self.machine.trigger(random.sample(self.states,1))
+    def go_idle(self):
+        """
+        do more work or fetch more data else stay idle
+        :return:
+        """
+        if (len(self.users_stack) > 0) or (len(self.posts_stack) >0):
+            self.machine.trigger('do_work')
+        else:
+            if self.nomore_data is False:
+                self.machnie.trigger('do_get_data')
 
-    def do_follow(self):
+        #we arrive at a permanent state of the FSM, we stay idle forever
+
+    def go_follow(self):
         """
         execute the actions needed to follow a user
         :return:
         """
         print("doing the follow action")
 
-    def do_unfollow(self):
+    def go_unfollow(self):
         """
         execute the actions needed to unfollow a user
         :return:
@@ -118,11 +143,13 @@ class FSMSession(Object):
         self.followed +=1
         #update the db
         #do all what is needed to do
+        print("action_delay")
 
         #clean up
         user=self.users_stack_follow.pop()
         if Setting.do_interact:
             self.users_stack_interact.append(user)
+            self.trigger("go_interact")
         #more actions possible here if needed
 
 
